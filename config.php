49a06056a2a6e35028e20f928249ee5693c89fcf
Colm Rourke <COLM.ROURKE.2014@mumail.ie>
2018-02-08 16:01:32 +0000

    Changes config

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'YOUR LOCATION');
-define('DB_USERNAME', 'YOUR USER NAME');
-define('DB_PASSWORD', 'YOUR PASSWORD');
-define('DB_NAME', 'YOUR DATABASE NAME');
+define('DB_USERNAME', 'crave');
+define('DB_PASSWORD', 'Password1');
+define('DB_NAME', 'emplyees');
  
 /* Attempt to connect to MySQL database */
 try{
