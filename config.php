eae809466ba221625ccd43cc11b997d884be7c2c
Colm Rourke <COLM.ROURKE.2014@mumail.ie>
2018-02-08 16:51:36 +0000

    Changes config

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'crude');
 define('DB_PASSWORD', 'Squier94');
-define('DB_NAME', 'employees');
+define('DB_NAME', 'crude');
  
 /* Attempt to connect to MySQL database */
 try{
